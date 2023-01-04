ef17ceb5ee767a16ea2bada780688c95d46e6ece
Christopher Hill <cjameshill@gmail.com>
2023-01-04 17:56:36 +0100

    Added loader

--- src/views/EmailView.vue
+++ src/views/EmailView.vue
@@ -1,16 +1,25 @@
 <script setup>
 import { onMounted, ref } from "vue";
 import { useLogin } from "@/composables/login";
+import SpinnerIcon from "@/components/icons/SpinnerIcon.vue";
 const { readMail } = useLogin();
 const emails = ref([]);
+const loading = ref(true);
 onMounted(async () => {
   const { response = {} } = await readMail();
   emails.value = response.value;
+  loading.value = false;
 });
 </script>
 
 <template>
   <main>
+    <div v-if="loading"
+      class="fixed top-0 left-0 flex backdrop-blur-md justify-center items-center w-full h-full bg-white bg-opacity-80">
+      <div class="flex justify-center items-center">
+        <SpinnerIcon class="h-14 w-14 fill-slate-500 animate-spin"></SpinnerIcon>
+      </div>
+    </div>
     <div class="flex flex-col space-y-8">
       <div v-if="emails.length > 0" v-for="email in emails" class="flex bg-gray-50 p-4 rounded-xl">
         <div class="flex flex-col space-y-2" :class="{ 'font-bold': !email.isRead }">
