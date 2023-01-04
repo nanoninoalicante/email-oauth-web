a841756f141c35369b406100c5681a79f21ef4e3
Christopher Hill <cjameshill@gmail.com>
2023-01-04 18:03:24 +0100

    Hide email button when not logged in

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup>
+import { useLogin } from "@/composables/login";
+const { userData } = useLogin();
 </script>
 
 <template>
   <main>
     <div class="container mx-auto flex justify-center">
-      <a href="/email" class="p-4 rounded-2xl text-md bg-blue-500 text-white">
+      <a v-if="userData" href="/email" class="p-4 rounded-2xl text-md bg-blue-500 text-white">
         View Emails
       </a>
     </div>
