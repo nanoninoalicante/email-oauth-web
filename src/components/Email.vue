1f3c96a3d1c478ac27ddf895af943882df3e4f34
Christopher Hill <cjameshill@gmail.com>
2023-02-23 11:24:50 +0000

    Added admin email view

---
+++ src/components/Email.vue
@@ -0,0 +1,33 @@
+<script setup>
+const props = defineProps(["email"]);
+const email = props.email;
+</script>
+<template>
+    <div
+        class="flex flex-col w-full space-y-2"
+        :class="{ 'font-bold': !email.isRead }"
+    >
+        <div class="flex flex-col py-2 px-4 bg-gray-100 rounded-2xl">
+            <span class="text-md"
+                >From: {{ email.from?.emailAddress?.name }}</span
+            >
+            <span class="text-sm"
+                >From: {{ email.from?.emailAddress?.address }}</span
+            >
+            <span v-for="to in email.toRecipients" class="text-sm"
+                >To: {{ to.emailAddress?.address }}</span
+            >
+        </div>
+        <div class="px-2">
+            <a
+                :href="email.webLink"
+                target="_blank"
+                class="text-xl hover:underline cursor-pointer"
+                >{{ email.subject }}</a
+            >
+        </div>
+        <div class="px-2">
+            <p class="text-xs">{{ email.bodyPreview }}</p>
+        </div>
+    </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import EmailView from "../views/EmailView.vue";
+import AdminEmailView from "../views/AdminEmailView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: "/email",
       name: "email",
       component: EmailView,
+    },
+    {
+      path: "/admin/email",
+      name: "admin-email",
+      component: AdminEmailView,
     },
   ],
 });
